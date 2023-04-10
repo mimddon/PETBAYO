d4d72f2305932b34076716bea5e13900b5c7bb18
mimddon <james990828@naver.com>
2023-04-10 15:47:40 +0900

    basic

---
+++ src/main/java/com/petbayo/petbayo/PetbayoApplication.java
@@ -0,0 +1,13 @@
+package com.petbayo.petbayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbayoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetbayoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petbayo/petbayo/PetbayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petbayo.petbayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetbayoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
