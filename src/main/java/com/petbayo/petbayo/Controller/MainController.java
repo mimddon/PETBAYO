e4d5c4e0e0c188bb3b4834439fbc6feb660a0acb
mimddon <129667682+mimddon@users.noreply.github.com>
2023-04-12 10:09:19 +0900

    2023-04-12-10:09

---
+++ src/main/java/com/petbayo/petbayo/Controller/MainController.java
@@ -0,0 +1,14 @@
+package com.petbayo.petbayo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+}
