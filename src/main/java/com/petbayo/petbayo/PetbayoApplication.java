1498339cf232b2029a4923b33c3e5b745d3358c8
cih6 <inhachoe6@gmail.com>
2023-04-27 13:37:25 +0900

    2023-04-27-14:36 build.gradle 및 PetbayoApplication 수정 putty 업로드

--- src/main/java/com/petbayo/petbayo/PetbayoApplication.java
+++ src/main/java/com/petbayo/petbayo/PetbayoApplication.java
@@ -2,9 +2,16 @@ package com.petbayo.petbayo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetbayoApplication {
+public class PetbayoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PetbayoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PetbayoApplication.class, args);
