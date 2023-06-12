45706386e36dafad55bd277619f462b58d6a93c0
mimddon <james990828@naver.com>
2023-06-13 03:56:58 +0900

    2023-06-13 3:56

--- src/main/java/com/petbayo/petbayo/Service/UserServiceImpl.java
+++ src/main/java/com/petbayo/petbayo/Service/UserServiceImpl.java
@@ -6,8 +6,14 @@ import org.springframework.stereotype.Service;
 
 import com.petbayo.petbayo.Dao.UserDao;
 import com.petbayo.petbayo.Model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.UUID;
+
 
 @AllArgsConstructor
 @Service
@@ -32,6 +38,10 @@ public class UserServiceImpl implements UserService {
             user.setNickname(item.getNickname());
             user.setBirth(item.getBirth());
             user.setGender(item.getGender());
+            user.setUserImage(item.getUserImage());
+            user.setImagePath(item.getImagePath());
+            user.setCarePoint(item.getCarePoint());
+            user.setUserIntro(item.getUserIntro());
 
             return true;
         } else
@@ -61,8 +71,59 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User findOne(String nickname) {
-        return dao.findOne(nickname);
+    public User findOne(int userId) {
+        return dao.findOne(userId);
+    }
+
+    @Override
+    public void uploadProfile(User user, MultipartFile file) throws IOException {
+        String uploadPath = "C:/img/userProfiles";
+        File directory = new File(uploadPath);
+
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        int index = file.getOriginalFilename().indexOf(".");
+        String extend = file.getOriginalFilename().substring(index);
+
+        String fileName = UUID.randomUUID().toString().replaceAll("-", "").substring(0,12);
+        String fullname = fileName + extend;
+        String nickname = String.valueOf(user.getNickname());
+
+        File uploadDir = new File(uploadPath, nickname);
+
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+
+        File saveFile = new File(uploadDir.getPath(), fullname);
+
+        file.transferTo(saveFile);
+
+        user.setUserImage(fullname);
+
+        String localPath = "C:\\img\\userProfiles\\" + user.getNickname() + "\\";
+        user.setImagePath(localPath + fullname);
+
+        dao.updateProfile(user);
+
+        if (user.getUserImage() != null) {
+            deleteOldFiles(uploadDir.getPath());
+        }
+
+    }
+    private void deleteOldFiles(String directoryPath) {
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+
+        if (files != null && files.length > 1) {
+            Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
+
+            for (int i = 1; i < files.length; i++) {
+                files[i].delete();
+            }
+        }
     }
 
 
